0e806453bdc1257bedbdc780bac2bcf32fc41d42
pradeep <pbpradeeptechno@gmail.com>
2021-04-13 22:41:32 +0530

    contentful and dev dependencies added to pacakge json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ContentfulService } from './common/services/contentful.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private contentfulService: ContentfulService) { }
+
   title = 'homeCusineApp';
+
+  ngOnInit() {
+    this.contentfulService.getPages().then(products => {
+      console.log(products);
+      debugger;
+    });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  contentFul: {
+    space: "yj0w64e1tfvc",
+    deliveryAccessToken: "6qVYGVkUoaxmnctoZW8NK2w1Iayek6MJ2Pyju5QDS8Q",
+    previewAccessToken: "IKM-_bOJ5fQiATkZzVdMTJtnPhAU7m3_zFANwhdJAb4"
+  }
 };
 
 /*
